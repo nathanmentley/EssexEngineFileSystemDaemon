184524224c4befaf9b51122619b4562fe1a43632
Nathan Mentley <nathanmentley@gmail.com>
2017-09-19 06:18:00 -0700

    new project structure. Setting up v1 of unit testing pattern.

---
+++ test/Test1.cpp
@@ -0,0 +1,6 @@
+#include <EssexEngineCore/catch.h>
+
+TEST_CASE("A test case 1")
+{
+    REQUIRE( 1 == 1 );
+}
---
+++ test/Test2.cpp
@@ -0,0 +1,7 @@
+#include <EssexEngineCore/catch.h>
+
+TEST_CASE("A test case 2")
+{
+    REQUIRE( 1 == 1 );
+    REQUIRE( 2 == 2 );
+}
